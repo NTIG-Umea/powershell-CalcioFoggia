bd947946125a5b2f28827887b944674b23c6d132
Edwin Rönnlund <edwinronnlund06@outlook.com>
2023-09-28 13:31:35 +0200

    Ip loop

--- ip.ps1
+++ ip.ps1
@@ -1,6 +1,6 @@
 for ($i = 1; $i -lt 256; $i++){
     $ping = Test-Connection "10.80.44.$i" -Quiet -Count 1;
-    IF ($ping -eq $true) {
+    IF ($ping) {
         Write-Output "10.80.44.$i"
     }
 }
